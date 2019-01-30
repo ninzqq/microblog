db87a5eef17a572783ef3b269cdd335cec468340
Janne Niinisalo <janne.niinisalo@gmail.com>
2019-01-30 16:45:25 +0200

    database section done, but shell context processor not added

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class User(db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
-        return '<User {}'.format(self.username)
+        return '<User {}>'.format(self.username)
         
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -20,4 +20,4 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
-        return '<Post {}'.format(self.body)
+        return '<Post {}>'.format(self.body)
